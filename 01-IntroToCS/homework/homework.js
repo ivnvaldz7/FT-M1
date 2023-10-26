35498d7eff7fc46e43b2b5c1c4933f9acec3cf90
ivnvldz7 <ivnvaldz@gmail.com>
2023-10-26 14:20:30 -0300

    Terminé los buclesgit add .

--- 01-IntroToCS/homework/homework.js
+++ 01-IntroToCS/homework/homework.js
@@ -1,10 +1,24 @@
-'use strict';
+"use strict";
 
-function BinarioADecimal(num) {}
+function BinarioADecimal(num) {
+  var decimal = 0;
+  var reverse = num.split("").reverse().join("");
+  for (let i = 0; i < reverse.length; i++) {
+    decimal += reverse[i] * 2 ** i;
+  }
+  return decimal;
+}
 
-function DecimalABinario(num) {}
+function DecimalABinario(num) {
+  let binario = "";
+  while (num > 0) {
+    binario += num % 2;
+    num = Math.floor(num / 2);
+  }
+  return binario.split('').reverse().join('');
+}
 
 module.exports = {
-   BinarioADecimal,
-   DecimalABinario,
+  BinarioADecimal,
+  DecimalABinario,
 };
