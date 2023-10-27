b19ca5f36d94057459269ed7fd15d63b0321cb14
Ivan <ivnvaldz@gmail.com>
2023-10-26 21:37:39 -0300

    Borrar esto

--- 01-IntroToCS/homework/homework.js
+++ 01-IntroToCS/homework/homework.js
@@ -1,10 +1,16 @@
-'use strict';
+"use strict";
 
-function BinarioADecimal(num) {}
+function BinarioADecimal(num) {
+  let decimal = 0;
+  for (let i = 0; i < num.length; i++) {
+    decimal += (num[i] * 2) ** i;
+  }
+  return decimal;
+}
 
 function DecimalABinario(num) {}
 
 module.exports = {
-   BinarioADecimal,
-   DecimalABinario,
+  BinarioADecimal,
+  DecimalABinario,
 };
